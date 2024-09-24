eae85e03adc48cfe38316e33e29fbee488241896
MarcusWentz <markzcollege404@gmail.com>
2024-09-24 17:02:13 -0400

    Foundry import Chainlink libraries operator.sol compiles

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # scroll-alchemy-level-up-2024
+
+## Foundry 
+
+:warning: Note: you might need to add libraries in forge with remappings.txt :warning:
+
+## Install Chainlink libraries
+```
+forge install smartcontractkit/chainlink-brownie-contracts --no-commit
+```
--- src/Operator.sol
+++ src/Operator.sol
@@ -1,9 +1,10 @@
 // // SPDX-License-Identifier: MIT
-// pragma solidity 0.7.6;
-// import "@chainlink/contracts/src/v0.7/Operator.sol";
-
 pragma solidity 0.8.19;
 // From GitHub: 
 // https://github.com/smartcontractkit/chainlink/blob/develop/contracts/src/v0.8/operatorforwarder/Operator.sol
-// Remix IDE
-import {Operator} from "@chainlink/contracts/src/v0.8/operatorforwarder/Operator.sol";
+// // Remix IDE
+// import {Operator} from "@chainlink/contracts/src/v0.8/operatorforwarder/Operator.sol";
+// Foundry 
+import {Operator} from "chainlink/v0.8/operatorforwarder/Operator.sol";
+
+
