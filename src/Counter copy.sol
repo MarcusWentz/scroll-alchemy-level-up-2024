2a96d74d735327dd7e51fd7011a71cae4cebefb3
MarcusWentz <markzcollege404@gmail.com>
2024-09-24 16:43:06 -0400

    Remix IDE test Operator.sol Solidity 0.8.19

---
+++ src/Counter copy.sol
@@ -0,0 +1,8 @@
+// // SPDX-License-Identifier: MIT
+// pragma solidity 0.7.6;
+// import "@chainlink/contracts/src/v0.7/Operator.sol";
+
+pragma solidity 0.8.19;
+// From GitHub: 
+// https://github.com/smartcontractkit/chainlink/blob/develop/contracts/src/v0.8/operatorforwarder/Operator.sol
+import "@chainlink/contracts/src/v0.8/operatorforwarder/Operator.sol";
