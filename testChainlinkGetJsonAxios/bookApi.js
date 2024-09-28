4e656aa654f973ca9b1717a806bf8b5faf369b52
MarcusWentz <markzcollege404@gmail.com>
2024-09-28 02:26:46 -0400

    HarryPotterObamaSonic10Inu book requests Axios working

--- testChainlinkGetJsonAxios/bookApi.js
+++ testChainlinkGetJsonAxios/bookApi.js
@@ -1,17 +1,25 @@
 const axios = require("axios");
 
-getJsonValues()
+const harryPotterBookISBN = "0590353427";
+const obamaBookISBN = "1524763169";
+const sonicBookISBN = "1879794902";
+const tenISBN = "0688135749";
+const inuBookISBN = "0764147439";
 
-async function getJsonValues() {
+getJsonValuesFromISBN(harryPotterBookISBN)
+getJsonValuesFromISBN(obamaBookISBN)
+getJsonValuesFromISBN(sonicBookISBN)
+getJsonValuesFromISBN(tenISBN)
+getJsonValuesFromISBN(inuBookISBN)
+
+async function getJsonValuesFromISBN(inputISBN) {
 
     // let baseUrl = "http://127.0.0.1:8080/"
     // // Filter interface:
     let baseUrl = "https://www.googleapis.com/books/v1/volumes?q=isbn:";
-
-    const sonicBookISBN = 1879794902;
-    let sonicBookUrl = baseUrl + sonicBookISBN.toString();
-    console.log(sonicBookUrl);
-    let responseRawJSON = await axios.get(sonicBookUrl);
+    let urlWithInputISBN = baseUrl + inputISBN;
+    console.log(urlWithInputISBN);
+    let responseRawJSON = await axios.get(urlWithInputISBN);
     let responseDataJSON = responseRawJSON.data;
     // console.log(responseRawJSON)
     // console.log(responseDataJSON)
