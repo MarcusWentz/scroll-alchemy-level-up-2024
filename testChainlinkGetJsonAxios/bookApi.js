a2b0dbb5d41132d25ea70d987f25bb27773c7c57
MarcusWentz <markzcollege404@gmail.com>
2024-09-27 20:55:29 -0400

    Sonic book Axios API GET example

--- testChainlinkGetJsonAxios/bookApi.js
+++ testChainlinkGetJsonAxios/bookApi.js
@@ -6,18 +6,17 @@ async function getJsonValues() {
 
     // let baseUrl = "http://127.0.0.1:8080/"
     // // Filter interface:
-    // https://userclub.opendatasoft.com/explore/dataset/les-eruptions-volcaniques-dans-le-monde/api/?disjunctive.country&refine.country=Iceland&refine.date=1727%2F08%2F03
-	let baseUrl = 
-    "https://userclub.opendatasoft.com/api/explore/v2.1/catalog/datasets/les-eruptions-volcaniques-dans-le-monde/records?limit=20&refine=country%3A%22Iceland%22&refine=date%3A%221727%2F08%2F03%22";
-    let responseRawJSON = await axios.get(baseUrl);
+    let baseUrl = "https://www.googleapis.com/books/v1/volumes?q=isbn:";
+
+    const sonicBookISBN = 1879794902;
+    let sonicBookUrl = baseUrl + sonicBookISBN.toString();
+    console.log(sonicBookUrl);
+    let responseRawJSON = await axios.get(sonicBookUrl);
     let responseDataJSON = responseRawJSON.data;
-    console.log(responseRawJSON)
-    console.log(responseDataJSON)
-    console.log(responseDataJSON.results[0])
-    console.log(responseDataJSON.results[0].country)
-    console.log(responseDataJSON.results[0].date)
-    console.log(responseDataJSON.results[0].coordinates)
-    console.log(responseDataJSON.results[0].coordinates.lat)
-    console.log(responseDataJSON.results[0].coordinates.lon)
+    // console.log(responseRawJSON)
+    // console.log(responseDataJSON)
+    console.log(responseDataJSON.items[0].volumeInfo.title)
+    console.log(responseDataJSON.items[0].volumeInfo.authors[0])
+    console.log(responseDataJSON.items[0].volumeInfo.imageLinks.thumbnail)
 
 }
